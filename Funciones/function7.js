0ac41381923f38590c958d56f2a2cc3e66493b6d
albertosantfaj <alberto.sant.faj@hotmail.com>
2023-07-07 13:33:40 +0200

    ejercicio

--- Funciones/function7.js
+++ Funciones/function7.js
@@ -1,20 +1,21 @@
 //Buscador de nombres
 
 const nameFinder = [
-    'Peter',
-    'Steve',
-    'Tony',
-    'Natasha',
-    'Clint',
-    'Logan',
-    'Xabier',
-    'Bruce',
-    'Peggy',
-    'Jessica',
-    'Marc'
-  ];
-  function finderName(array) {
-    let busqueda = array.includes('Natasha');
-    return busqueda;
-  }
-  console.log(finderName(nameFinder));
+  "Peter",
+  "Steve",
+  "Tony",
+  "Natasha",
+  "Clint",
+  "Logan",
+  "Xabier",
+  "Bruce",
+  "Peggy",
+  "Jessica",
+  "Marc",
+];
+function finderName(array, name) {
+  let busqueda = array.includes(name);
+  console.log(array);
+  return busqueda;
+}
+console.log(finderName(nameFinder, "Peter"));
