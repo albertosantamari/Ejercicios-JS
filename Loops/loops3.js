9bba3d863043ee624c462a15079d29cede0ccef7
albertosantfaj <alberto.sant.faj@hotmail.com>
2023-07-10 10:46:28 +0200

    ejercicios

---
+++ Loops/loops3.js
@@ -0,0 +1,7 @@
+//Probando For...of
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+for (iteracion of placesToTravel){
+    console.log(iteracion);
+}
